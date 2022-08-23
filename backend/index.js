52a6ca477b309910f25bb29420c8333896fa865c
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-08-23 19:14:35 +0530

    added form

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 require('./db');
-
+const cors = require('cors')
 const userRouter = require("./router/user");
 const app = express();
 
 const port = process.env.PORT || 8000
 
+app.use(cors());
 
 //before logic it works
 app.use(express.json())
--- backend/router/user.js
+++ backend/router/user.js
@@ -12,5 +12,8 @@ router.post('/signin', signin)
 router.post('/verify-email', verifyEmail)
 router.post('/forgot-password', forgetPassword)
 router.post('/reset-password', isResetTokenValid, resetPassword)
+router.get('/verify-token', isResetTokenValid, (req, res) => {
+    res.json({ success: true })
+})
 
 module.exports = router
