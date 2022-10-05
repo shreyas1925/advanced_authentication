e3007992b32cd8f9a1d379bd760ace3d8212bbe3
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-10-06 01:11:06 +0530

    donw with routing and signup page

--- auth/Components/Signup.js
+++ auth/Components/Signup.js
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
 import { Formik } from 'formik'
+import * as yup from 'yup'
 
 const initialValues = {
     name: "",
@@ -8,7 +9,14 @@ const initialValues = {
     password: ""
 }
 
+const validationSchema = yup.object({
+    name: yup.string().trim().required("Name is required").min(3, "Name should be atleast 3 characters"),
+    email: yup.string().trim().required("Email is required").email("Email is invalid"),
+    password: yup.string().trim().required("Password is required").min(8, "Password should be atleast 6 characters")
+})
+
 const Login = () => {
+
 
     const navigation = useNavigation();
     const navigateToLogin = () => {
@@ -18,32 +26,52 @@ const Login = () => {
     const navigateToForget = () => {
         navigation.navigate("ForgetPassword");
     }
+
+    const handleSignup = (values, formikActions) => {
+
+        setTimeout(() => {
+            console.log(values, formikActions);
+            formikActions.resetForm();
+            formikActions.setSubmitting(false);
+
+        }, 3000)
+
+    }
     return (
         <KeyboardAvoidingView style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Image style={styles.logo} source={require("../assets/logo.jpg")} />
-                <Formik initialValues={initialValues} validationSchema={ }>
+                <Formik initialValues={initialValues} validationSchema={validationSchema}
+                    onSubmit={handleSignup}>
                     {
-                        () => (
-                            <>
-                                <TextInput style={styles.input} placeholder="shreyas" />
-                                <TextInput style={styles.input} placeholder="shreyasshettigar@gmail.com" />
-                                <TextInput style={styles.input} placeholder="password" />
+                        ({ errors, values, touched, isSubmitting, handleSubmit, handleChange, handleBlur }) => {
+                            console.log(values);
+                            return (
+                                <>
+                                    <Text style={styles.error}>{touched.name && errors.name ? errors.name : ''}</Text>
+                                    <TextInput style={styles.input} onChangeText={handleChange('name')} onBlur={handleBlur('name')} placeholder="shreyas" />
 
-                                <Pressable style={styles.btnContainer}>
-                                    <Text style={styles.text}>Signup</Text>
-                                </Pressable>
-                                <View style={styles.linkcontainer}>
-                                    <Pressable>
-                                        <Text style={styles.linkText} onPress={navigateToLogin}>Login</Text>
-                                    </Pressable>
-                                    <Pressable>
-                                        <Text style={styles.linkText} onPress={navigateToForget}>Forgot password ?</Text>
-                                    </Pressable>
+                                    <Text style={styles.error}>{touched.email && errors.name ? errors.email : ''}</Text>
+                                    <TextInput style={styles.input} onChangeText={handleChange('email')} onBlur={handleBlur('email')} placeholder="shreyasshettigar@gmail.com" />
 
-                                </View>
-                            </>
-                        )
+                                    <Text style={styles.error}>{touched.password && errors.name ? errors.password : ''}</Text>
+                                    <TextInput style={styles.input} secureTextEntry onChangeText={handleChange('password')} onBlur={handleBlur('password')} placeholder="password" />
+
+                                    <Pressable style={[styles.btnContainer, { backgroundColor: isSubmitting ? 'gray' : '#8469cf' }]} onPress={handleSubmit}>
+                                        <Text style={styles.text}>Signup</Text>
+                                    </Pressable>
+                                    <View style={styles.linkcontainer}>
+                                        <Pressable>
+                                            <Text style={styles.linkText} onPress={navigateToLogin}>Login</Text>
+                                        </Pressable>
+                                        <Pressable>
+                                            <Text style={styles.linkText} onPress={navigateToForget}>Forgot password ?</Text>
+                                        </Pressable>
+
+                                    </View>
+                                </>
+                            )
+                        }
                     }
                 </Formik>
 
